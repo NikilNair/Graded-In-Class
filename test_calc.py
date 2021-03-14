86f1f3b34ebcdad36e198d10cd6680275be3efb4
NikilNair <nairni@oregonstate.edu>
2021-03-13 22:54:04 -0800

    unit tests

---
+++ test_calc.py
@@ -0,0 +1,14 @@
+"""
+Unit testing the calculator app
+"""
+
+import calc
+
+
+class TestCalculator:
+
+    def test_add(self):
+        assert 5 == calc.add(1, 4)
+
+    def test_subtract(self):
+        assert 2 == calc.subtract(5, 3)
